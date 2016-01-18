aa08a254de0175c2ca7891ae6a6038698dc814df
trabalho <trabalho@10.3.1.163>
2016-01-18 18:29:30 -0300

    Serviço baseado em dados definido pelo programador.

--- crud-jax-ws-service/src/br/com/ws/crud/jaxws/servico/SIBNoticia.java
+++ crud-jax-ws-service/src/br/com/ws/crud/jaxws/servico/SIBNoticia.java
@@ -1,63 +1,63 @@
 package br.com.ws.crud.jaxws.servico;
-
+ 
 import java.sql.SQLException;
 import java.util.ArrayList;
-
+ 
 import javax.jws.WebService;
-
+ 
 import br.com.ws.crud.jaxws.dao.NoticiaDao;
 import br.com.ws.crud.jaxws.modelo.Noticia;
-
+ 
 @WebService(endpointInterface = "br.com.ws.crud.jaxws.servico.SEINoticia")
 public class SIBNoticia implements SEINoticia{
-
-	public boolean criar(Noticia noticia) {
-		NoticiaDao dao = new NoticiaDao();
-		try {
-			return dao.criar(noticia);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-
-	public boolean alterar(Noticia noticia) {
-		NoticiaDao dao = new NoticiaDao();
-		try {
-			return dao.alterar(noticia);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-
-	public boolean deletar(Noticia noticia) {
-		NoticiaDao dao = new NoticiaDao();
-		try {
-			return dao.deletar(noticia);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-
-	public Noticia ler(int id) {
-		NoticiaDao dao = new NoticiaDao();
-		try {
-			return dao.ler(id);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	public ArrayList<Noticia> listar() {
-		NoticiaDao dao = new NoticiaDao();
-		try {
-			return dao.listar();
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
+ 
+    public boolean criar(Noticia noticia) {
+        NoticiaDao dao = new NoticiaDao();
+        try {
+            return dao.criar(noticia);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+ 
+    public boolean alterar(Noticia noticia) {
+        NoticiaDao dao = new NoticiaDao();
+        try {
+            return dao.alterar(noticia);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+ 
+    public boolean deletar(Noticia noticia) {
+        NoticiaDao dao = new NoticiaDao();
+        try {
+            return dao.deletar(noticia);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+ 
+    public Noticia ler(int id) {
+        NoticiaDao dao = new NoticiaDao();
+        try {
+            return dao.ler(id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+ 
+    public ArrayList<Noticia> listar() {
+        NoticiaDao dao = new NoticiaDao();
+        try {
+            return dao.listar();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
--- crud-jax-ws-service/src/br/com/ws/crud/jaxws/servico/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://servico.jaxws.crud.ws.com.br/")
-package br.com.ws.crud.jaxws.servico;
